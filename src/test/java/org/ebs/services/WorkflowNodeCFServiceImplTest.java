f3706caeda168dfa9634ae22b36a21acc4da1b0e
jarojas <jalberto.roj@gmail.com>
2020-08-25 15:35:23 -0500

    added utest

--- src/test/java/org/ebs/services/WorkflowNodeCFServiceImplTest.java
+++ src/test/java/org/ebs/services/WorkflowNodeCFServiceImplTest.java
@@ -3,10 +3,16 @@ package org.ebs.services;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
+import org.ebs.model.WorkflowCFTypeModel;
+import org.ebs.model.WorkflowModel;
 import org.ebs.model.WorkflowNodeCFModel;
 import org.ebs.model.repos.EntityReferenceRepository;
 import org.ebs.model.repos.HtmlTagRepository;
@@ -15,12 +21,18 @@ import org.ebs.model.repos.WorkflowCFValueRepository;
 import org.ebs.model.repos.WorkflowNodeCFRepository;
 import org.ebs.model.repos.WorkflowNodeRepository;
 import org.ebs.services.to.WorkflowNodeCFTo;
+import org.ebs.services.to.WorkflowNodeTo;
+import org.ebs.util.Connection;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.convert.ConversionService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @ExtendWith(MockitoExtension.class)
 public class WorkflowNodeCFServiceImplTest {
@@ -72,6 +84,30 @@ public class WorkflowNodeCFServiceImplTest {
         Optional<WorkflowNodeCFTo> result = subject.findWorkflowNodeCF(0);
         assertThat(result.isEmpty()).isTrue();
 
+    }
+
+    @Test
+    public void givenNoArguments_whenFindWorkflowNodeCFs_thenReturnConnection() {
+        List<WorkflowNodeCFModel> content = Arrays.asList(new WorkflowNodeCFModel(), new WorkflowNodeCFModel());
+        Pageable pageable = PageRequest.of(0, 50);
+
+        Connection<WorkflowNodeCFModel> connection = new Connection<>(content, pageable, 2);
+
+        when(mockWorkflownodecfRepository.findByCriteria(any(), any(), any(), any()))
+            .thenReturn(connection);
+        when(mockConverter.convert(any(), any()))
+            .thenReturn(new WorkflowNodeCFTo());
+            
+        Page<WorkflowNodeCFTo> result = subject.findWorkflowNodeCFs(null, null, null);
+
+        assertThat(result).isNotNull();
+        assertThat(result.getTotalElements()).isEqualTo(2);
+
+        verify(mockWorkflownodecfRepository, times(1))
+            .findByCriteria(any(), any(), any(), any());
+        verify(mockConverter, times(2)) 
+            .convert(any(), any());
+        
     }
 
 }
